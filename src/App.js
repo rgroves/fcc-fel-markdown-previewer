5e6c43d85ba18409bfddbf83a825528552448893
Robert Groves <rwgdev@gmail.com>
2021-02-07 13:56:21 -0600

    Add textarea for markdown text.
    This completes:
    User Story #1: I can see a textarea element with a corresponding id="editor".

--- src/App.js
+++ src/App.js
@@ -3,19 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Markdown Previewer</h1>
+      <textarea id="editor" rows="25" cols="80" />
     </div>
   );
 }
