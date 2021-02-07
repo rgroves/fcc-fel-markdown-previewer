022426d15607a3a5eca4eb3f3c25a2c403940231
Robert Groves <rwgdev@gmail.com>
2021-02-07 14:05:26 -0600

    Add preview div.
    This completes:
    User Story #2: I can see an element with a corresponding id="preview".

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Markdown Previewer</h1>
       <textarea id="editor" rows="25" cols="80" />
+      <div id="preview"></div>
     </div>
   );
 }
