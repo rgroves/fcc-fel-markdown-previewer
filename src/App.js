6c358c5b876df33b94d35c52c634bd1941375639
Robert Groves <rwgdev@gmail.com>
2021-02-08 09:15:49 -0600

    Add default markdown for initial display.
    This completes:
    User Story #5: When my markdown previewer first loads, the default text in the
    id=editor field should contain valid markdown that represents at least one of
    each of the following elements: a header (H1 size), a sub header (H2 size), a
    link, inline code, a code block, a list item, a blockquote, an image, and bolded
    text.
    
    User Story #6: When my markdown previewer first loads, the default markdown in
    the id=editor field should be rendered as HTML in the #preview element.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PreviewWindow from "./PreviewWindow";
 import marked from "marked";
 import "./App.css";
 
+const defaultRawMarkdown = `Markdown Example
+================
+
+GitHub Flavored Markdown (GFM)
+------------------------------
+
+Read more about [GFM](https://github.github.com/gfm/).
+
+The formatted HTML above was originally styled using the Markdown below:
+\`\`\`
+Markdown Example
+================
+
+GitHub Flavored Markdown (GFM)
+------------------------------
+
+Read more about [GFM](https://github.github.com/gfm/)
+\`\`\`
+
+Here is an example of something that would be treated with \`*emphasis*\`.
+
+Here is an example of **strong text** versus *emphasized text*.
+
+### Facts About Markdown
+1. Markdown is a lightweight markup language for creating formatted text using a plain-text editor
+2. John Gruber and Aaron Swartz created Markdown in 2004
+3. Markdown is meant to be appealing to human readers in its source code form
+
+> "For months I’ve been working with John Gruber on a new project. The idea was 
+> to make writing simple web pages, and especially weblog entries, as easy as 
+> writing an email, by allowing you to use much the same syntax and converting it
+> automatically into HTML." — Aaron Swartz (posted March 19, 2004)
+
+![Markdown is made for humans](http://source.unsplash.com/2LowviVHZ-E/480x270)`;
+
 function App() {
   const [rawMarkdown, setRawMarkdown] = useState("");
   const [formattedMarkdown, setFormattedMarkdown] = useState("");
+
+  useEffect(() => {
+    setRawMarkdown(defaultRawMarkdown);
+    setFormattedMarkdown(marked(defaultRawMarkdown));
+  }, []);
 
   function updateRawMarkdown(event) {
     const newRawMarkdown = event.target.value;
